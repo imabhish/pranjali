8380cca7741fb230833bdcd47e48109de231c19a
abhishek sharma <imabhish143@gmail.com>
2024-03-16 10:12:04 +0530

    certificate

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,3 +1,6 @@
+import certificate from '../assets/png/cert.jpg'
+
+
 export const achievementData = {
     bio : "The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. - Alvin Toffler",
     achievements : [
@@ -7,7 +10,7 @@ export const achievementData = {
             details : 'A ethical hacking essential course from EC-Council',
             date : 'Feb 19, 2024',
             field : '297528',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-0cd075aa-f55b-4d9f-aec0-119020a4e792.jpg?v=1611846350000'
+            image : certificate
         }
 
     ]
